988929d707048388d2d98e6c19070453858dccb9
Jiu-Ling <2388997861@qq.com>
2021-10-18 22:44:41 +0800

    Add night_Checkin

---
+++ checkin_night.py
@@ -0,0 +1,7 @@
+from libdailyClock import *
+
+if __name__ == '__main__':
+    # 账户为  http://authserver.cumt.edu.cn/authserver/login 账户
+    username = ''
+    password = ''
+    checkin_main(username, password)
